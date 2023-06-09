f99543a6d6722464aebc13732ff24a50a2c05292
Eduardo Garcia Camacho <eduardo.camacho@coppel.com>
2023-06-08 17:41:45 -0700

    Aplicacion de paises v1

---
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { Country } from '../../interfaces/country';
+import { CountriesService } from '../../services/countries.service';
+
+@Component({
+  selector: 'app-by-region-page',
+  templateUrl: './by-region-page.component.html',
+  styles: [
+  ]
+})
+export class ByRegionPageComponent {
+  countries:Country[]=[];
+  constructor(private countriesService:CountriesService){}
+  searchRegion(region:string):void{
+    this.countriesService.searchRegion(region)
+    .subscribe(countries =>{
+      this.countries=countries;
+    })
+  }
+
+}
